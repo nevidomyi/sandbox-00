4fdc16ff42c421d6a3b73c1584b979b13173a086
nevidomyi <47025875+nevidomyi@users.noreply.github.com>
2024-06-10 21:19:20 +0300

    chore: Update npm dependencies and add Mongoose module for database connection

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,12 @@
-import { Module } from '@nestjs/common';
+import { Module, OnApplicationBootstrap } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { UsersService } from './users/users.service';
 import { UsersController } from './users/users.controller';
 import { UsersModule } from './users/users.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import databaseConfig from './database/config/database.config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { MongooseHealthIndicator, TerminusModule } from '@nestjs/terminus';
 
 @Module({
   imports: [
@@ -13,10 +15,26 @@ import databaseConfig from './database/config/database.config';
       load: [databaseConfig],
       envFilePath: ['.env'],
     }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('database.url'),
+      }),
+      inject: [ConfigService],
+    }),
+    TerminusModule,
     AuthModule,
     UsersModule,
   ],
+
   controllers: [UsersController],
   providers: [UsersService],
 })
-export class AppModule {}
+export class AppModule implements OnApplicationBootstrap {
+  constructor(private readonly health: MongooseHealthIndicator) {}
+
+  async onApplicationBootstrap() {
+    const healthCheck = await this.health.pingCheck('database');
+    console.log(healthCheck);
+  }
+}
